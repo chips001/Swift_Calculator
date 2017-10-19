32528baa67ba249332358f034520c34600a64a54
h-ichiki <h-ichiki@clinks.jp>
2017-10-19 17:50:41 +0900

    [modify]2017/10/19時点最新

--- Swift_Calculator/ViewController.swift
+++ Swift_Calculator/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Expression
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mFormulaLbl: UILabel!
+    @IBOutlet weak var mAnswerLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mFormulaLbl.text = ""
+        mAnswerLbl.text = ""
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,102 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    /// 「C」「=」以外のButton押下時の処理
+    ///
+    /// - Parameter sender: UIButtonのインスタンス
+    @IBAction func mInputFormulaAction(_ sender: UIButton) {
+        //
+        guard let formulaText = mFormulaLbl.text else {
+            //nilだったらreturn
+            return
+        }
+        
+        guard let senderedText = sender.titleLabel?.text else {
+            //「sender.titleLabel?.text」:Buttonのタイトルを取得する
+            //nilだったらreturn
+            return
+        }
+        
+        mFormulaLbl.text = formulaText + senderedText
+        print("formulaText:\(formulaText)  senderedText:\(senderedText)")
+        
+    }
 
+    
+    /// 「C」Button押下時の処理
+    ///
+    /// - Parameter sender: UIButtonの処理
+    @IBAction func mClearFormulaAction(_ sender: UIButton) {
+        mFormulaLbl.text = ""
+        mAnswerLbl.text = ""
+    }
+    
+    
+    /// 「=」Bitton押下時の処理
+    ///
+    /// - Parameter sender: UIButtonの処理
+    @IBAction func mCalculateAnswerAction(_ sender: UIButton) {
+        
+        guard let formulaText = mFormulaLbl.text else {
+            //nilだったらreturn
+            return
+        }
+        
+        //入力された値の判定
+        let formula:String = formulaFormat(formulaText)
+        //判定結果より計算した答えを表示orMsgの表示
+        mAnswerLbl.text = evalFormula(formula)
+        
+    }
+    
+    
+    /// 入力された整数に「.0」を追加して少数として追加する
+    /// また、「÷」を「/」へ、「×」を「*」へ変換する
+    ///
+    /// - Parameter formulaText: 入力された値
+    /// - Returns: フォーマット後の値
+    private func formulaFormat (_ formulaText:String) -> String{
+        
+        //replacingOccurrences:文字列の置換
+        let formattedFormula: String = formulaText.replacingOccurrences(
+            of: "(?<=^|[÷×\\+\\-\\(])([0-9]+)(?=[÷×\\+\\-\\)]|$)",
+            with: "$1.0",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil
+        ).replacingOccurrences(of: "÷", with: "/").replacingOccurrences(of: "×", with: "*")
+        
+        return formattedFormula
+    }
+    
+    
+    /// Expressionフレームワークで文字列の計算式を評価して答えを求める
+    ///
+    /// - Parameter formula:入力された式
+    /// - Returns: 計算の答えor不当な式であった場合のMsg
+    private func evalFormula(_ formula:String) -> String{
+        do{
+            let expression = Expression(formula)
+            let answer = try expression.evaluate()
+            return formatAnswer(String(answer))
+        
+        }catch{
+            //計算式が不当だった場合
+            return "Please Formula"
+        }
+    }
+    
+    /// 答えの小数点以下が「.0」だった場合は、「.0」を削除して答えを整数で表示する
+    ///
+    /// - Parameter answer: 実際に計算される式
+    /// - Returns: 計算の答え
+    private func formatAnswer(_ answer:String) -> String{
+        let formattedeAnswer:String = answer.replacingOccurrences(
+            of: "\\.0$",
+            with: "",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil)
+        return formattedeAnswer
+    }
 }
 
